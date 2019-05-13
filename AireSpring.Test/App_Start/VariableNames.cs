846aa4d2abd693e295bfc5937de75bd60607eab2
absalonabiada <50601072+absalonabiada@users.noreply.github.com>
2019-05-14 07:42:56 +0800

    Add files via upload

---
+++ AireSpring.Test/App_Start/VariableNames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AireSpring.Test.App_Start
+{
+    public class Vars
+    {
+        public static string Mode = "mode";
+
+        public static string EmployeeId = "EmployeeId";
+
+        public static string ModeCreate = "create";
+
+        public static string ModeEdit = "edit";
+
+        public static string ConnectionStringName = "DefaultConnection";
+
+        public static string SessionCurrentEmployee = "CurrentEmployee";
+
+        public static string SessionEmployeesList = "EmployeesList";
+
+        public static string SearchString = "SearchText";
+    }
+}
